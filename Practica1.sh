79e52075417cdbd24cd53d791cd969e7b5e1a41f
Albertovdc <114888921+Albertovdc@users.noreply.github.com>
2024-03-13 23:52:59 -0600

    Update Practica1.sh

--- Practica1.sh
+++ Practica1.sh
@@ -6,3 +6,7 @@ tail -n 15 /var/log/syslog | sort -r > syslog.txt 2>/dev/null
 #Tail indica la cola -n delimita las lineas deseadas
 # | hace que la salida sea la nueva entrada
 # sort ordena 
+
+# Problema 2
+a=10
+b=$((2*10))
