e9f715cde84eab67e0ff6746cbc4429d4ce6ec3a
Albertovdc <114888921+Albertovdc@users.noreply.github.com>
2024-03-13 23:54:29 -0600

    Update Practica1.sh

--- Practica1.sh
+++ Practica1.sh
@@ -8,5 +8,18 @@ tail -n 15 /var/log/syslog | sort -r > syslog.txt 2>/dev/null
 # sort ordena 
 
 # Problema 2
+# Formato para una operacion aritmetica
+# $((expresion))
 a=10
+echo "$a"
 b=$((2*10))
+echo "$b"
+c=$(((a+b)/2))
+echo "$c"
+d=$(((2+3)*10))
+echo "$d"
+#bc -> Usamos un comando externo que nos permite imprimir decimales
+div=$(echo "scale=2; $c /2" | bc)
+echo "$div"
+res=$((b%c))
+echo "$res"
