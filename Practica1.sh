a60e0d0275f53669938f28c4b95c8e7e903578a4
ArturoOlguin <158232301+ArturoOlguin@users.noreply.github.com>
2024-03-13 23:51:30 -0600

    Update Practica1.sh
    Se ingresa la resolución del problema 1

--- Practica1.sh
+++ Practica1.sh
@@ -1,1 +1,8 @@
+#! /bin/bash
 
+#Problema1
+
+tail -n 15 /var/log/syslog | sort -r > syslog.txt 2>/dev/null
+#Tail indica la cola -n delimita las lineas deseadas
+# | hace que la salida sea la nueva entrada
+# sort ordena 
